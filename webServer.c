8cf21e7943a6cfe824ddf32a9bb6ff3a39eeefb0
Sophia Davis <scdgrapefruit@gmail.com>
2014-07-29 12:09:22 -0400

    Refactor with help from Beej

--- webServer.c
+++ webServer.c
@@ -6,7 +6,8 @@
 #include<sys/socket.h>    
 #include<sys/stat.h>    
 #include<sys/types.h>    
-#include<unistd.h> 
+#include<unistd.h>  
+#include <netdb.h>
 
 int main(int argc, char *argv[]){
     
@@ -16,20 +17,32 @@ int main(int argc, char *argv[]){
     int bufsize = 1024;
     char *buffer = malloc(bufsize);
     
+    struct sockaddr_in address_in; 
+    address_in.sin_family = AF_INET;
+    address_in.sin_port = htons(15000);
+    address_in.sin_addr.s_addr = INADDR_ANY;
+    
+    struct addrinfo address;
+    memset(&address, 0, sizeof(address));
+    address.ai_family = AF_INET;
+    address.ai_socktype = SOCK_STREAM;
+    address.ai_protocol = 0;
+    address.ai_addr = (struct sockaddr *) &address_in;
+    
+    struct addrinfo *results;
+    
+    // Socket address information 
+    int status = getaddrinfo(INADDR_ANY, "15000", &address, &results);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+    
 // 1. Create socket
-    listening_socket = socket(AF_INET, SOCK_STREAM, 0); // IPv4, stream, 0 = choose correct protocol for stream vs datagram 
+    listening_socket = socket(results->ai_family, results->ai_socktype, results->ai_protocol); // IPv4, stream, 0 = choose correct protocol for stream vs datagram 
     if (listening_socket > 0) { // success!
         printf("Socket created.\n");
     }
-    
-    // Socket address information
-    struct sockaddr_in address; // could use getaddrinfo() -- http://beej.us/guide/bgnet/output/html/multipage/syscalls.html)
-    address.sin_family = AF_INET; // address family
-    address.sin_addr.s_addr = INADDR_ANY; // internet address (4-byte IP address (in Network Byte Order))
-                           // INADDR_ANY makes socket listen at any available interfaces
-    address.sin_port = htons(15000); // port number -- htons = host to network short
-        // direction of reading in data (kernel expects network address, 15000 is human readable)
-    printf("port: %i\n", address.sin_port);  
     
     // Allow reuse of port -- from Beej
     int yes = 1;
@@ -39,14 +52,17 @@ int main(int argc, char *argv[]){
     }  
     
 // 2. Bind socket to address (host and port)
-    if (bind(listening_socket, (struct sockaddr *) &address, sizeof(address)) == 0) { // socket id, *sockaddr struct w address info, length (in bytes) of address
-                                                                
-        printf("Binding socket\n");
+    if (bind(listening_socket, results->ai_addr, results->ai_addrlen) == 0) { // socket id, *sockaddr struct w address info, length (in bytes) of address                                                         
+        printf("Binding socket...\n");
+    }
+    else {
+        printf("I'm not binding!\n");
     }
     
 // 3. Listen for connections
     while (1) {
         if (listen(listening_socket, 10) < 0) { // socketfd and backlog (# requests kept waiting)
+            printf("I'm not listening!\n");
             perror("server: listen");
             exit(1);
         }
@@ -54,8 +70,10 @@ int main(int argc, char *argv[]){
 // 4. Accept clients
         // Now we have a new socket specifically for sending/receiving data w this client
         // Info about incoming connection goes into &address
-        if ((new_socket = accept(listening_socket, (struct sockaddr *) &address, &addrlen)) < 0) {
+        addrlen = sizeof((struct sockaddr *) &address_in);
+        if ((new_socket = accept(listening_socket, (struct sockaddr *) &results->ai_addr, &addrlen)) < 0) {
             perror("server: accept");
+            printf("I'm not accepting!\n");
             exit(1);
         }
         
@@ -65,7 +83,7 @@ int main(int argc, char *argv[]){
         
 // 5. Send and receive data
         // Clear buffer
-        memset(buffer, 0, sizeof(buffer));
+        memset(buffer, 0, bufsize);
         
         recv(new_socket, buffer, bufsize, 0);
         printf("%s\n", buffer);
