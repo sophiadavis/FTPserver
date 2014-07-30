9085c5d75567e09f322229f600f2ba685ec00e15
Sophia Davis <scdgrapefruit@gmail.com>
2014-07-30 16:54:30 -0400

    Successfully created new threads to handle clients

--- webServer.c
+++ webServer.c
@@ -9,6 +9,10 @@
 #include<unistd.h>  
 #include <netdb.h>
 
+#include <pthread.h>
+
+void* process_connection(void *sock);
+
 int main(int argc, char *argv[]){
     
     int listening_socket, new_socket;
@@ -61,17 +65,20 @@ int main(int argc, char *argv[]){
     }
     
 // 3. Listen for connections
+    pid_t pID;
     while (1) {
+        
         if (listen(listening_socket, 10) < 0) { // socketfd and backlog (# requests kept waiting)
             printf("I'm not listening!\n");
             perror("server: listen");
             exit(1);
         }
-        
+    
 // 4. Accept clients
+    
+    // Make a new socket specifically for sending/receiving data w this client
+    // Info about incoming connection goes into sockaddr_storage struct 
         
-        // Make a new socket specifically for sending/receiving data w this client
-        // Info about incoming connection goes into sockaddr_storage struct 
         struct sockaddr_storage client;
         socklen_t addr_size = sizeof(client);
         if ((new_socket = accept(listening_socket, (struct sockaddr *) &client, &addr_size)) < 0) {
@@ -79,44 +86,61 @@ int main(int argc, char *argv[]){
             printf("I'm not accepting!\n");
             exit(1);
         }
-        
+    
         if (new_socket > 0) {
-            printf("\nA client has connected.\n");
+            pthread_t tid;
+            pthread_create(&tid, NULL, &process_connection, &new_socket);
+            printf("\nA client has connected, new thread created.\n");
         }
-        
-// 5. Send and receive data
-        int bufsize = 1024;
-        char *buffer = malloc(bufsize);
-        int bytes_received;
-        int len, bytes_sent;
-        
-        while (1) {
-//             printf("--Server starting while loop.--\n");
-            memset(buffer, 0, bufsize); // Clear buffer
-            
-            bytes_received = recv(new_socket, buffer, bufsize, 0);
 
-            if (bytes_received > 0) {
-                printf("Server received: %s (%i bytes)\n", buffer, bytes_received);
-                len = strlen(buffer);
-                bytes_sent = send(new_socket, buffer, len, 0);
-                if (bytes_sent < 0) {
-                    close(new_socket);
-                    perror("client: connect");
-                    exit(0);
-                }
-            }
-            else {
-                close(new_socket);
-                perror("client: connect");
-                exit(0);
-                break;
-            }
-        }
-        free(buffer);
+        
     }
     freeaddrinfo(results);
     close(new_socket);
     close(listening_socket);
     return 0;
+}
+
+void *process_connection(void *sock) {
+    int *new_socket_ptr;
+    int new_socket;
+    
+    // cast void* as int*
+    new_socket_ptr = (int *) sock;
+    
+    // get int from int*
+    new_socket = *new_socket_ptr;
+        
+// 5. Send and receive data
+    int bufsize = 1024;
+    char *buffer = malloc(bufsize);
+    int bytes_received;
+    int len, bytes_sent;
+    
+    while (1) {
+//             printf("--Server starting while loop.--\n");
+            
+        memset(buffer, 0, bufsize); // Clear buffer
+        bytes_received = recv(new_socket, buffer, bufsize, 0);
+        
+        if (bytes_received > 0) {
+            printf("Server received: %s (%i bytes)\n", buffer, bytes_received);
+            len = strlen(buffer);
+            bytes_sent = send(new_socket, buffer, len, 0);
+            if (bytes_sent < 0) {
+                close(new_socket);
+                printf("here1");
+                perror("client: connect");
+                exit(0);
+            }
+        }
+        else {
+            close(new_socket);
+            printf("here2");
+            perror("client: connect");
+            exit(0);
+            break;
+        }
+    }
+    free(buffer);
 }
