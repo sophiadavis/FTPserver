e83c7bcb144fd45be955a4d58cd28a200ebd7dae
Sophia Davis <scdgrapefruit@gmail.com>
2014-08-07 11:15:10 -0400

    About to refactor to allow non-text file transfers

--- ftpServer.c
+++ ftpServer.c
@@ -21,39 +21,13 @@ int begin_connection(int listening_socket, void *on_create_function);
 
 // Commands
 const char *USER = "USER";
-        // USER <SP> <username> <CRLF>
-        // Login as user username. 
-        // If the username is "anonymous", reply with 230. Otherwise, reply with 530.
 const char *QUIT = "QUIT"; 
-        // QUIT <CRLF>
-        // Quit
-        // Return 221. The server now knows that nobody is logged in.
-const char *PWD = "PWD";  // "pwd";//
-        //PWD <CRLF>
-        // Print working directory.
-        // Reply with 257 (and include the working directory as the string following the reply number).
+const char *PWD = "PWD";
 const char *CWD = "CWD"; 
-        // CWD <SP> <pathname> <CRLF>
-        // Change working directory.
-        // You may assume that pathname will be a pathname that is either ".." or "." or a relative pathname that does not include ".." or ".". 
-        // If the requested action can be successfully completed, reply with 250. Otherwise, reply with 550.
 const char *PORT = "PORT";
-        // PORT <SP> <host-port> <CRLF>
-        //Specifies the port to be used for the data connection.
-        // READ MORE Create a new socket (for the data connection) given the host address and the port number. Reply with 200 to indicate success. Reply with 500 to indicate failure.
 const char *NLST = "NLST";
-        // NLST [<SP> <pathname>] <CRLF>
-        // List files in the current directory or files specified by the optional arguments.
-        // First, indicate that a data communication transfer is starting with reply 125. Next, transfer the file names over the data communication channel that was previously opened by the client having issued a PORT command. (You may assume that the client will have issued the PORT command.) Once the action is successfully completed, reply with 226. In case of error, reply with 450. In both cases close the data communication channel upon completion.
-
 const char *RETR = "RETR";
-        // RETR <SP> <pathname> <CRLF>
-        // Retrieve a file specified by path pathname.
-        // You should assume that the client has called PORT to initialize the data communication channel. If no file has been specified for retrieval, reply with 450. If a file has been specified, first send reply 125. Next, if the specified file exists, is a file, and is readable, send the file byte by byte over using the data communication. If this action is completed successfully, reply with 250. Otherwise, reply with 550. In both cases, close the data communication channel upon completion.
-
 const char *TYPE = "TYPE";
-        // TYPE <Transfer mode> <CRLF>
-        // simply reply with 200
 const char *SYST = "SYST";
 const char *FEAT = "FEAT";
 const char *PASV = "PASV";
@@ -65,11 +39,11 @@ const char *ROOT = "/var/folders/r6/mzb0s9jd1639123lkcsv4mf00000gn/T/server";
 
 int main(int argc, char *argv[]){
     
-    // Set directory of server
+    // Set root directory of server
     int chdir_status = chdir(ROOT);
     check_status(chdir_status, "chdir");
-    int chroot_status = chroot(ROOT);
-    check_status(chroot_status, "chroot");
+//     int chroot_status = chroot(ROOT);
+//     check_status(chroot_status, "chroot");
     
     // For storing results from creating socket
     struct addrinfo *results;
@@ -251,9 +225,7 @@ int process_request(char *buffer, int new_socket, int bytes_received, int *sign_
         }
         else if (strcmp(parsed[0], CWD) == 0) {
             ///////////////////////////////////////////
-            // TODO keep client from going above root????
-            ///////////////////////////////////////////
-            // ALSO, THREADS SHARE WORKING DIRECTORY, DAMMIT.
+            // THREADS SHARE WORKING DIRECTORY, DAMMIT.
             ///////////////////////////////////////////
             
             int chdir_status = chdir(parsed[1]);
@@ -266,7 +238,7 @@ int process_request(char *buffer, int new_socket, int bytes_received, int *sign_
             }
         }
         else if (strcmp(parsed[0], NLST) == 0) {
-            // http://stackoverflow.com/questions/4204666/how-to-list-files-in-a-directory-in-a-c-program
+            // Thanks to http://stackoverflow.com/questions/4204666/how-to-list-files-in-a-directory-in-a-c-program
             int bytes_data_written = 0;
             int bytes_response_code_written = 0;
             DIR *d;
@@ -276,16 +248,15 @@ int process_request(char *buffer, int new_socket, int bytes_received, int *sign_
             bytes_response_code_written = snprintf(data, data_size, "%s", "150 Here comes the directory listing. \r\n");
             if (d && *accept_data_socket > 0) {
                 while ((dir = readdir(d)) != NULL) {
-  //                 snprintf(data_line, 15, "%s\r\n", dir->d_name);
-  //                 printf("%s\n", data_line);
-  //                 send(new_socket, &data_line, 20, 0);
-                
                     bytes_data_written = bytes_data_written + snprintf(data_over_second_connection + bytes_data_written, data_size - bytes_data_written, "%s \r\n", dir->d_name);
-                
                 }
                 closedir(d);
+                
+                // Send directory information immediately over data socket, then close connection
                 bytes_sent += send(*accept_data_socket, data_over_second_connection, strlen(data_over_second_connection), 0);
                 close(*accept_data_socket);
+                *accept_data_socket = 0;
+                
                 snprintf(data + bytes_response_code_written, data_size, "226 Directory send OK.\r\n");
             }
             else {
@@ -313,7 +284,6 @@ int process_request(char *buffer, int new_socket, int bytes_received, int *sign_
             bytes_response_code_written = snprintf(data, data_size, "150 Opening ASCII mode data connection for %s (178 bytes). \r\n", parsed[1]);
             if ((fp != NULL) && *accept_data_socket > 0) {
               
-              
                 char fileBuf[1000];
                 char data_over_second_connection[10000];
                 strncpy(data_over_second_connection,"", 1);
@@ -321,9 +291,10 @@ int process_request(char *buffer, int new_socket, int bytes_received, int *sign_
                 while (fgets(fileBuf, 1000, fp) != NULL) { // while we haven't reached EOF
                     bytes_data_written = bytes_data_written + snprintf(data_over_second_connection + bytes_data_written, data_size - bytes_data_written, "%s", fileBuf);
                 }
+                fclose(fp);
                 printf("%s", data_over_second_connection);
                 
-                bytes_sent += send(*accept_data_socket, data_over_second_connection, strlen(data_over_second_connection), 0);
+                
                 
 //                 int a;
 //                 char *letter;
@@ -333,10 +304,14 @@ int process_request(char *buffer, int new_socket, int bytes_received, int *sign_
 //                     putchar(letter);
 //                     bytes_sent += send(*accept_data_socket, letter, 1, 0);
 //                 }
-                
+
+                // Send directory information immediately over data socket, then close connection
+                bytes_sent += send(*accept_data_socket, data_over_second_connection, strlen(data_over_second_connection), 0);
                 close(*accept_data_socket);
+                *accept_data_socket = 0;
+                
                 snprintf(data + bytes_response_code_written, data_size, "226 Transfer complete.\r\n");
-                fclose(fp);
+                
             }
             else {
                 snprintf(data, data_size, "%s", "550 RETR error\n");
@@ -360,15 +335,13 @@ int process_request(char *buffer, int new_socket, int bytes_received, int *sign_
         else if (strcmp(parsed[0], PASV) == 0) {
             CURRENT_CONNECTION_PORT++;
             *data_port = CURRENT_CONNECTION_PORT;
-            struct addrinfo *data_results; // TODO free this guy?????
+            struct addrinfo *data_results;
             *listening_data_socket = prepare_socket(*data_port, data_results);
             
             
             // Client expects (a1,a2,a3,a4,p1,p2), where port = p1*256 + p2
             int p1 = *data_port / 256;
             int p2 = *data_port % 256;
-//             int p1 = 5000 / 256;
-//             int p2 = 5000 % 256;
             
             int listen_status = listen(*listening_data_socket, 10);
             check_status(listen_status, "listen");
@@ -386,12 +359,7 @@ int process_request(char *buffer, int new_socket, int bytes_received, int *sign_
             check_status(*accept_data_socket, "accept");
             printf("\naccepted, DATA connection began\n");
             
-//             pthread_t tid;
-//             NUM_THREADS++;
-//             pthread_create(&tid, NULL, &process_data_connection, data_connection_socket);
-            
-            // snprintf(data, data_size, "%s =127,0,0,1,%i,%i\n", "227 Entering Passive Mode", p1, p2);
-//             freeaddrinfo(data_results);
+            freeaddrinfo(data_results);
         }
         else if (strcmp(parsed[0], PORT) == 0) {
             snprintf(data, data_size, "%s\n", "200 Port command ok");
@@ -407,11 +375,6 @@ int process_request(char *buffer, int new_socket, int bytes_received, int *sign_
         bytes_sent = send(new_socket, data, strlen(data), 0);
     }
     printf("Data sent: %s\n", data);
-    
-    // Clear out parsed arguments
-//     for(z = 0; z < num_args; z++) {
-//         memset(parsed[z], 0, strlen(parsed[z]));
-//     }
     
     free(data);
     return bytes_sent;     
